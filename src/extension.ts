f274e34121f82e83185416cb56844bf8dd82d868
shuhan.wang <shuhan.wang@amh-group.com>
2023-07-20 09:56:04 +0800

    feat: 支持显示url地址

--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,16 @@ import * as qrcode from 'qrcode';
 export function activate(context: vscode.ExtensionContext) {
   let panel: vscode.WebviewPanel | undefined;
 
-  let disposable = vscode.commands.registerCommand('extension.generateQRCode', () => {
+  let disposable = vscode.commands.registerCommand('extension.generateQRCode', (uri) => {
     if (!panel) {
       // 创建一个Webview面板
       panel = vscode.window.createWebviewPanel(
         'qrCodeGenerator',
-        'QR Code Generator',
-        vscode.ViewColumn.One,
+        '小贷二维码生成器',
+        vscode.ViewColumn.One, // 不拆分编辑器
         {
-          enableScripts: true
+          enableScripts: true,
+          retainContextWhenHidden: true,
         }
       );
 
@@ -23,7 +24,7 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     // 获取Webview的HTML内容
-    panel.webview.html = getWebviewContent(panel.webview);
+    panel.webview.html = getWebviewContent(uri?.path);
 
     // 监听Webview发出的消息
     panel.webview.onDidReceiveMessage((message) => {
@@ -33,7 +34,6 @@ export function activate(context: vscode.ExtensionContext) {
             .then((data) => {
               // 生成二维码后，将二维码图片路径发送给Webview
               panel?.webview.postMessage({ command: 'showQRCode', imagePath: data });
-              vscode.window.showErrorMessage(data);
             })
             .catch((error) => {
               vscode.window.showErrorMessage('Failed to generate QR Code.');
@@ -60,20 +60,91 @@ async function generateQRCode(text: string): Promise<string> {
   });
 }
 
-function getWebviewContent(webview: vscode.Webview) {
+function getWebviewContent(path: string) {
+  const display = path ? 'block' : 'none';
+  const devBaseUrl = 'https://devstatic.ymm56.com/microweb/#/mw-loan-h5';
+  const qaBaseUrl = 'https://qastatic.ymm56.com/microweb/#/mw-loan-h5';
+  const prodBaseUrl = 'https://static.ymm56.com/microweb/#/mw-loan-h5';
+  let devPath = '';
+  let devContainerPath = '';
+  let qaPath = '';
+  let qaContainerPath = '';
+  let prodPath = '';
+  let prodContainerPath = '';
+  if (path) {
+    devPath = devBaseUrl + path.substring(path.indexOf('/mw-loan-h5/src/pages') + 21, path.length - 4);
+    devContainerPath = 'ymm://view/web?url=' + encodeURIComponent(devPath);
+    qaPath = qaBaseUrl + path.substring(path.indexOf('/mw-loan-h5/src/pages') + 21, path.length - 4);
+    qaContainerPath = 'ymm://view/web?url=' + encodeURIComponent(qaPath);
+    prodPath = prodBaseUrl + path.substring(path.indexOf('/mw-loan-h5/src/pages') + 21, path.length - 4);
+    prodContainerPath = 'ymm://view/web?url=' + encodeURIComponent(prodPath);
+  }
   return `
     <!DOCTYPE html>
     <html>
     <head>
       <title>Generate QR Code</title>
       <style>
-        body { font-family: Arial, sans-serif; }
-        input { width: 100%; padding: 5px; font-size: 16px; margin-bottom: 10px; }
-        button { width: 100%; padding: 10px; font-size: 16px; }
+        .base {
+          font-size: 18px;
+          line-height: 24px;
+          color: #e5c785;
+          margin-bottom: 12px;
+        }
+        .dev {
+          
+        }
+        .qa {
+          
+        }
+        .prod {
+          
+        }
+        .divider {
+          height: 2px;
+          width: 50%;
+          background-color: #960505;
+          margin: 20px 0;
+        }
+        input { 
+          width: 90%;
+          padding: 12px; 
+          font-size: 16px;
+          border: none;
+          outline: none;
+          margin-bottom: 10px;
+        }
+        input:focus {
+          border: none;
+          outline: none;
+        }
+        button { 
+          width: 90%; 
+          padding: 16px; 
+          font-size: 16px; 
+          background: #FFD338;
+          border-radius: 12px;
+          border: none;
+          cursor: pointer;
+        }
+        button:active {
+          background: #f0af22;
+        }
         #qrCodeContainer { text-align: center; margin-top: 20px; }
       </style>
     </head>
     <body>
+      <div style="display: ${display}">
+        <div class="base dev">dev环境: ${devPath}</div>
+        <div class="base dev">容器地址: ${devContainerPath}</div>
+        <div class="divider"></div>
+        <div class="base qa">qa环境: ${qaPath}</div>
+        <div class="base qa">容器地址: ${qaContainerPath}</div>
+        <div class="divider"></div>
+        <div class="base prod">prod环境: ${prodPath}</div>
+        <div class="base prod">容器地址: ${prodContainerPath}</div>
+      </div>
+
       <input id="urlInput" type="text" placeholder="Enter a URL">
       <button onclick="generateQRCode()">Generate QR Code</button>
       <div id="qrCodeContainer"></div>
